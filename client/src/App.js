0c4e66013f403ab9b621f6ac8935012c44ffc59b
vinay1723 <vinaykumargoud.17july@gmail.com>
2024-08-29 13:26:19 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./Header";
+import Hero from "./Hero";
+import Answers from "./Answers";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Answers />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
